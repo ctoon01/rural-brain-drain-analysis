6da44713536c329f1be458c75190f3d2e306c970
ctoon01 <christopher.ray.toon@gmail.com>
2026-06-18 15:11:20 -0700

    Update 02_data_cleaning.sql

--- sql/02_data_cleaning.sql
+++ sql/02_data_cleaning.sql
@@ -1,1 +1,10 @@
+-- 02_data_cleaning.sql
+-- Cleans imported data and creates usable join fields
 
+UPDATE census_education
+SET college_plus_pct_num = college_plus_pct::NUMERIC / 100;
+
+UPDATE bls_employment
+SET fips =
+    LPAD(state_fips_code::TEXT, 2, '0') ||
+    LPAD(county_fips_code::TEXT, 3, '0');
