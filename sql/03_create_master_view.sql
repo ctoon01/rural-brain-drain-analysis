65f518802dea87cb526704d857410c6df20e360d
ctoon01 <christopher.ray.toon@gmail.com>
2026-06-18 15:13:23 -0700

    Update 03_create_master_view.sql

--- sql/03_create_master_view.sql
+++ sql/03_create_master_view.sql
@@ -1,1 +1,30 @@
+-- 03_create_master_view.sql
+-- Creates master joined county-level analysis view
 
+DROP VIEW IF EXISTS v_county_master;
+
+CREATE VIEW v_county_master AS
+SELECT
+    u.fips,
+    u.state,
+    u.county_name,
+    u.population_2010,
+    u.rucc_2013,
+    CASE
+        WHEN u.rucc_2013 BETWEEN 1 AND 3 THEN 'Metro'
+        WHEN u.rucc_2013 BETWEEN 4 AND 6 THEN 'Micropolitan'
+        ELSE 'Rural'
+    END AS county_tier,
+    c.total_25_plus,
+    c.college_plus,
+    c.college_plus_pct_num AS college_plus_pct,
+    b.year,
+    b.labor_force::INTEGER AS labor_force,
+    b.employed::INTEGER AS employed,
+    b.unemployed::INTEGER AS unemployed,
+    b.unemployment_rate::NUMERIC AS unemployment_rate
+FROM usda_rucc u
+LEFT JOIN census_education c
+    ON u.fips = c.fips
+LEFT JOIN bls_employment b
+    ON u.fips = b.fips;
